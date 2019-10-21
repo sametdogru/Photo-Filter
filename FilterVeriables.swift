5f2dff193a9bedfdcee9701e823516d88c5d9b89
MacBookPro <smttdgru34@gmail.com>
2019-10-21 20:06:10 +0300

    fix

---
+++ FilterVeriables.swift
@@ -0,0 +1,35 @@
+//
+//  FilterVeriables.swift
+//  PhotoApp
+//
+//  Created by Ahmet Doğru on 25.09.2019.
+//  Copyright © 2019 Samet Doğru. All rights reserved.
+//
+
+import Foundation
+
+var CICategoryColorEffectNames = [
+    "Original",
+    "CIPhotoEffectChrome",
+    "CIPhotoEffectFade",
+    "CIPhotoEffectInstant",
+    "CIPhotoEffectNoir",
+    "CIPhotoEffectProcess",
+    "CIPhotoEffectTonal",
+    "CIPhotoEffectTransfer",
+    "CISepiaTone",
+    "CIPhotoEffectMono",
+    "CIFalseColor"]
+
+var CICategoryColorEffectTitle = [
+    "Original",
+    "Chrome",
+    "Fade",
+    "Instant",
+    "Noir",
+    "Processs",
+    "Tonal",
+    "Transfer",
+    "Sepia",
+    "Mono",
+    "False"]
